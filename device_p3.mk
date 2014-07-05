d9096600c3e7ff268add3efcc7fec50f49804ba7
Henrique <henrique@mowta.pt>
2014-07-05 21:21:56 +0100

    First commit

--- device_p3.mk
+++ device_p3.mk
@@ -20,5 +20,5 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_p3
+PRODUCT_NAME := p3
 PRODUCT_DEVICE := p3
